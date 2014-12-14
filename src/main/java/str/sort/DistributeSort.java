cabe1090c697b0a2f0860428e38aa3d6cc6a5fe0
vns <01_kilimandjaro@ukr.net>
2014-12-14 15:12:54 +0200

    --

--- src/main/java/str/sort/DistributeSort.java
+++ src/main/java/str/sort/DistributeSort.java
@@ -1,5 +1,15 @@
 package str.sort;
 
+/**
+ * 
+ * @author vns
+ *
+ * Effective for sorting small numbers.
+ * 
+ * Algoritms
+ * Robert Sedgewick and Kevin Wayne.
+ * 
+ */
 public class DistributeSort {
 
 }
